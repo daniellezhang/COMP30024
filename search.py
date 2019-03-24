16b07964292b29ec0919640d49ecfb1981afb1aa
DanielleZhang <liuxinzhang042@gmail.com>
2019-03-24 13:10:23 +1100

    add function that generate all possible actions

--- search.py
+++ search.py
@@ -2,29 +2,149 @@
 COMP30024 Artificial Intelligence, Semester 1 2019
 Solution to Project Part A: Searching
 
-Authors:
+Authors: Danielle Zhang & Ishan Sodhi
 """
 
 import sys
 import json
 
+
+"""dictionary of every colour's exiting coordinates"""
+exit_dict = {"red":[[3,-3],[3,-2],[3,-1],[3,0]],
+"green":[[-3,3],[-2,3],[-1,3],[-0,3]],
+"blue":[[-3,0],[-2,-1],[-1,-2],[0,-3]]}
+
 class Node(object):
     def __init__(self):
         self.children = []
-        '''data consists of the coordinates of every piece, and the operation(tuple)'''
         self.data = None
         self.cost = None
+        self.heuristic_distance = None
 
 class Operation(object):
-    def __init__(self):
-        self.previous_position = None
-        self.new_position = None
-        self.action = None
+    def __init__(self, pos1, pos2, action):
+        self.previous_position = pos1
+        self.new_position = pos2
+        self.action = action
+
+def ring_generator(position, ring_no = 1):
+
+    coordinates = []
+    x_coordinates = []
+    y_coordinates = []
+
+    #Code to generate the X coordinates
+    for i in range(position[0] - ring_no, position[0] + ring_no + 1):
+        x_coordinates.append(i)
+    for j in range(1, ring_no):
+        x_coordinates.append(i)
+    for k in range(i, position[0] - ring_no - 1, -1):
+        x_coordinates.append(k)
+    for j in range(1, ring_no):
+        x_coordinates.append(k)
+
+    #Code to generate the Y coordinates
+    for i in range(position[1], position[1] - ring_no - 1, -1):
+        y_coordinates.append(i)
+    for j in range(1, ring_no):
+        y_coordinates.append(i)
+    for k in range(i, position[1] + ring_no + 1):
+        y_coordinates.append(k)
+    for j in range(1, ring_no):
+        y_coordinates.append(k)
+    for l in range(k, position[1], -1):
+        y_coordinates.append(l)
+
+    #To combine X and Y
+    for i in range(0, len(x_coordinates)):
+        coordinates.append([x_coordinates[i], y_coordinates[i]])
+
+    return coordinates
+
+#Player will be the tuple of coordinates
+def neighbours(player):
+
+    possible_moves = []
+    forbidden_Coords = ring_generator([0, 0], 4)
+    moves = ring_generator(player)
+
+    for i in moves:
+        if not (i in forbidden_Coords):
+            possible_moves.append(i)
+
+    return possible_moves
+
+
+#Function that return a list of possible operations
+def possible_action(piece_index, pieces, blocks, colour):
+    current_piece = pieces[piece_index]
+    all_neighbours = neighbours(current_piece)
+    is_occupied = [False]*len(all_neighbours)
+    actions = []
+    #check if there is any neighbour that is occupied by other pieces
+    for i in range(len(pieces)):
+        if i != piece_index:
+            for j in range(len(all_neighbours)):
+                if all_neighbours[j] == pieces[i]:
+                    is_occupied[j] = True
+    for block in blocks:
+        for j in range(len(all_neighbours)):
+            if all_neighbours[j] == block:
+                is_occupied[j] = True
+
+    #check if the current piece is able to exit the board
+    exit_list = exit_dict[colour]
+    for pos in exit_list:
+        if pos == current_piece:
+            actions.append(Operation(current_piece,None,"EXIT"))
+
+
+    #go through the list of neighbours to find possible moves
+    for i in range(len(all_neighbours)):
+        neighbour = all_neighbours[i]
+        #neighbour is occupied. Check if a jump action is valid
+        if is_occupied[i]:
+            q_difference = neighbour[0]-current_piece[0]
+            r_difference = neighbour[1]-current_piece[1]
+            jump_pos = [neighbour[0]+q_difference, neighbour[1]+r_difference]
+            #check if the new position after a jump action is on the board
+            out_of_board = False
+            invalid_position = ring_generator([0,0],4)
+            for position in invalid_position:
+                if position == jump_pos:
+                    out_of_board = True
+                    break
+            # new position is on the board
+            if not out_of_board:
+                #check if there is other piece or block on this new_position
+                occupied = False
+                for i in range(len(pieces)):
+                    if pieces[i] == jump_pos:
+                        occupied = True
+                        break
+                if not occupied:
+                    for block in blocks:
+                        if block == jump_pos:
+                            occupied = True
+                            break
+                #position is not occupied. Jump action is valid
+                if not occupied:
+                    actions.append(Operation(current_piece,jump_pos,"JUMP"))
+        #neighbour not occupied. A move action is valid
+        else:
+            actions.append(Operation(current_piece, neighbour,"MOVE"))
+
+    return actions
+
 
 
 def main():
-    with open(sys.argv[1]) as file:
-        data = json.load(file)
+    """with open(sys.argv[1]) as file:
+        data = json.load(file)"""
+    actions = possible_action(1,[[0,0],[3,-3],[-2,1]],[[-1,-2],[-1,1],[1,1],[3,-1]],"red")
+    for action in actions:
+        print(action.action, action.previous_position, action.new_position)
+
 
     # TODO: Search for and output winning sequence of moves
     # ...
