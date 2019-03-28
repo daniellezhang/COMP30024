575e898fa431dee61fe59eda45997beed6756656
DanielleZhang <liuxinzhang042@gmail.com>
2019-03-29 10:15:53 +1100

    add print_board in A*

--- search.py
+++ search.py
@@ -207,6 +207,13 @@ def A_Star(positions,blocks, colour):
         if is_goal:
             break
 
+        board_dict = {}
+        for piece in current[1]:
+            board_dict[tuple(piece)] = 'r'
+            for piece in blocks:
+                board_dict[tuple(piece)] = 'b'
+        print_board(board_dict)
+
         for state in next_states(current[1], blocks, colour):
             new_position = state[0]
 
